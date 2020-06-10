03a784cf2681e7fb2e28c4a963dd07cf75b8b123
Rocky Chen <rocky.chen@dbca.wa.gov.au>
2020-06-10 15:25:54 +0800

    Catch and log the exception in nginx and rancher's archive command

--- nginx/command/archive.py
+++ nginx/command/archive.py
@@ -1,14 +1,21 @@
 import argparse
 from datetime import date,datetime
 import sys
+import traceback
+import logging
 
 import nginx
+
+logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser(prog="archive",description='Archive nginx configuration files and push it to blob storage')
 
 def run():
-    args = parser.parse_args(sys.argv[2:])
-    nginx.archive()
+    try:
+        args = parser.parse_args(sys.argv[2:])
+        nginx.archive()
+    except:
+        logger.error(traceback.format_exc())
     
 
 
--- rancher/command/archive.py
+++ rancher/command/archive.py
@@ -1,14 +1,21 @@
 import argparse
 from datetime import date,datetime
 import sys
+import traceback
+import logging
 
 import rancher
+
+logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser(prog="archive",description='Archive rancher configuration files and push it to blob storage')
 
 def run():
-    args = parser.parse_args(sys.argv[2:])
-    rancher.archive()
+    try:
+        args = parser.parse_args(sys.argv[2:])
+        rancher.archive()
+    except:
+        logger.error(traceback.format_exc())
     
 
 
