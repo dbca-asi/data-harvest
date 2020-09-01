f994b446e6a49cc18748794f1edc00917bb0be34
Rocky Chen <Rocky.Chen@dbca.wa.gov.au>
2020-09-01 09:39:40 +0800

    Adjust some env's default value

--- podstatus/settings.py
+++ podstatus/settings.py
@@ -12,8 +12,8 @@ RESOURCE_NAME = env("PODSTATUS_RESOURCE_NAME",vtype=str,default="containerstatus
 WORKSPACE = env("PODSTATUS_AZLOG_WORKSPACE",vtype=str,required=True)
 QUERY = env("PODSTATUS_AZLOG_QUERY",vtype=str,required=True)
 QUERY_DURATION = env("PODSTATUS_QUERY_DURATION",vtype=timedelta,required=True)# configure in seconds
-LOG_DELAY_TIME = env("PODSTATUS_DELAY_TIME",vtype=timedelta,default=timedelta(seconds=600))# configure in seconds
-MAX_ARCHIVE_TIME_PER_LOG = env("PODSTATUS_MAX_ARCHIVE_TIME_PER_LOG",vtype=int,default=3000)# configure in seconds
+LOG_DELAY_TIME = env("PODSTATUS_DELAY_TIME",vtype=timedelta,default=timedelta(seconds=300))# configure in seconds
+MAX_ARCHIVE_TIME_PER_LOG = env("PODSTATUS_MAX_ARCHIVE_TIME_PER_LOG",vtype=int,default=1500)# configure in seconds
 QUERY_START = env("PODSTATUS_QUERY_START",vtype=datetime,required=True)# configure in 'yyyy/mm/dd HH:mm:ss' in local time
 USER =  env("PODSTATUS_AZLOG_USER",vtype=str,required=True)
 PASSWORD =  env("PODSTATUS_AZLOG_PASSWORD",vtype=str,required=True)
